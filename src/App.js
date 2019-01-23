0a8402c0a22af51fc2d6c7a7f55026edb827b1f1
Satya Thiru <e138082@LUV.AD.SWACORP.COM>
2019-01-23 16:07:31 -0600

    updated default msg

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 		  <h1 className="App-title">Welcome to React</h1>
 		</header>
           <p>
-            To get started, edit <code>src/App.js</code> and save to Reload.
+            Edit <code>src/App.js</code> and save to Reload.
           </p>
         
       </div>
